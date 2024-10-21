da7511cf09c85fd947e9264ca49d760eab4cac51
moreym26 <moreym26@up.edu>
2024-10-21 10:38:27 -0700

    Cookie sound and destroy
    Added the chomp sound when you run over the cookie and destroys it when you run over the cookie.

--- Scripts/Candy.gd
+++ Scripts/Candy.gd
@@ -6,7 +6,22 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#Rotates Cookie
 	rotate_y(0.01)
 	
+
+
+func _on_body_entered(body: Node3D) -> void:
+	#Plays chomping sound when player runs over coin
+	$ChompSound.play()
+	#Destroys coins and plays music at same time makes it look nice
+	$MeshInstance3D.queue_free()
+
+
+
+func _on_chomp_sound_finished() -> void:
+	#After sound is played, destroy cookie
+	queue_free()
