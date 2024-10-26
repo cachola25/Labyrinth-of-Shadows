e72f8573e3d779553b2b2bb4662a47f48ab1e6d3
cachola25 <cachola25@up.edu>
2024-10-26 13:36:41 -0700

    Random Spawn for Monster
    Have the monster randomly spawn in behind the player

--- Scripts/Maze.gd
+++ Scripts/Maze.gd
@@ -1,21 +1,33 @@
 extends Node3D
 
+@onready var monster_scene = preload("res://scenes/Monster.tscn")
+var monster_in_scene = false
+
 func _ready() -> void:
 	pass
-	
+
 func _process(delta: float) -> void:
-	spawn_monster()
-	
+	if not monster_in_scene:
+		spawn_monster()
+		
 func spawn_monster():
-	var spawn_distance = 5.0
+	var spawn_distance = 5.0 
+	var random_offset_range = 2.0
 	var random_offset = Vector3(
-		randf() * 2 - 1,
+		(randf() * 2 - 1) * random_offset_range, 
 		0,
-		randf() * 2 - 1
+		(randf() * 2 - 1) * random_offset_range  
 	)
+
+	# Calculate the spawn position behind the player
 	var player_position = $player.global_transform.origin
-	var player_direction = -$player.global_transform.basis.z
+	var player_direction = -$player.global_transform.basis.z.normalized()
 	var spawn_position = player_position + (player_direction * spawn_distance) + random_offset
+
+	# Instantiate the monster and set its position
+	var monster_instance = monster_scene.instantiate()
+	monster_instance.global_transform.origin = spawn_position
+	add_child(monster_instance)
 	
-	
+	monster_in_scene = true
 	
--- player/player_helper.gd
+++ player/player_helper.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
-
+@onready var maze_scene = get_tree().root.get_child(0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +15,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	print(body)
 	if body is monster and $survivor/SpotLight3D.light_energy > 0:
 		body.queue_free()
+		maze_scene.monster_in_scene = false
