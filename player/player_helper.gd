fce3d1b8cb6a33e6304723601d5bbe94a24ad2f2
cachola25 <cachola25@up.edu>
2024-10-27 20:53:17 -0700

    Reworking monster and flashlight
    If the monster is too far from the player, speed up the movement.
    
    Reworked the check_line_of_sight function to use RayCast nodes instead of the physics server.

--- player/player_helper.gd
+++ player/player_helper.gd
@@ -4,6 +4,7 @@ extends Node3D
 @onready var player_scene = get_parent()
 @onready var monster_in_view = false
 @onready var monster_scene
+@onready var flashlight_rays = [$survivor/RayCast3D, $survivor/RayCast3D2, $survivor/RayCast3D3]
 
 func _ready() -> void:
 	pass
@@ -11,7 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if monster_in_view:
-		print("monster in view")
 		check_line_of_sight()
 	
 func _on_area_3d_body_entered(body: Node3D) -> void:
@@ -20,44 +20,18 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		monster_in_view = true
 
 func check_line_of_sight():
-	var from = player_scene.current_camera.global_transform.origin 
-	var forward = player_scene.current_camera.global_transform.basis.z.normalized()
-	var to = from + forward * $survivor/SpotLight3D.spot_range
-	var space_state = get_world_3d().direct_space_state
+	for i in range(len(flashlight_rays)):
+		if flashlight_rays[i].is_colliding():
+			if flashlight_rays[i].get_collider() is monster:
+				despawn_monster()
 
-	# Create a new PhysicsRayQueryParameters3D object
-	var query = PhysicsRayQueryParameters3D.new()
-	query.from = from
-	query.to = to
-	query.exclude = [self, player_scene.current_camera]
-	query.collision_mask = (1 << 2) | (1 << 3)
-
-	# Perform the raycast
-	var result = space_state.intersect_ray(query)
-	
-	if result:
-		var collider = result.collider
-		if collider is monster:
-			# Line of sight is clear; no obstacles between player and monster
-			print("Line of sight is clear; no obstacles between player and monster")
-			despawn_monster(monster_scene)
-		else:
-			# There is an obstacle between player and monster
-			print("There is an obstacle between player and monster")
-			print(collider)
-			pass
-	else:
-		despawn_monster(monster_scene)
-
-func despawn_monster(monster_scene):
+func despawn_monster():
 	if not monster_scene:
 		return
 	monster_scene.queue_free()
 	maze_scene.monster_in_scene = false
 	maze_scene.get_node("monster_spawn_timer").start()
 
-
 func _on_flashlight_area_body_exited(body: Node3D) -> void:
 	if body is monster or $survivor/SpotLight3D.light_energy <= 0:
 		monster_in_view = false
-		print("cant see monster anymore")
--- scripts/monster.gd
+++ scripts/monster.gd
@@ -2,19 +2,25 @@ extends CharacterBody3D
 
 class_name monster
 const SPEED = 3.0
-
+const SPEEDUP_THRESHOLD = 30
 @onready var player = get_parent().get_node("player")
 @export var turn_speed = 4.0
 
 func _ready() -> void:
 	pass
+
+func get_path_length():
+	var path = $NavigationAgent3D.get_current_navigation_path()
+	var total_distance = 0
+	for i in range(path.size() - 1):
+		total_distance += path[i].distance_to(path[i+1])
+	return total_distance
 	
 func _physics_process(delta):
 	$NavigationAgent3D.set_target_position(player.global_transform.origin)
-
+	get_path_length()
 	# Get the next position along the path
 	var next_position = $NavigationAgent3D.get_next_path_position()
-
 	if next_position != Vector3.ZERO:
 		# Calculate the direction to the next waypoint
 		var direction = (next_position - global_transform.origin).normalized()
@@ -26,7 +32,10 @@ func _physics_process(delta):
 		# Move forward in the facing direction
 		var forward = transform.basis.z
 		var velocity = forward * SPEED
-		set_velocity(velocity)
+		if get_path_length() > SPEEDUP_THRESHOLD:
+			set_velocity(10 * velocity)
+		else:
+			set_velocity(velocity)
 		move_and_slide()
 	else:
 		# No path found or at target position
