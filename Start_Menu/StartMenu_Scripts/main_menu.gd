41f96584e54a78606b8588de35c2ba0d4b3c7ff9
cachola25 <cachola25@up.edu>
2024-10-28 18:31:03 -0700

    Update menu scenes

--- Start_Menu/StartMenu_Scripts/main_menu.gd
+++ Start_Menu/StartMenu_Scripts/main_menu.gd
@@ -7,14 +7,12 @@ extends Node3D
 @export var game_scene: PackedScene
 
 func _ready():
-	start.pressed.connect(self.on_start_pressed)
-	quit.pressed.connect(self.on_quit_pressed)
 	fader.connect("fade_finished", self.on_fade_finished)
 
-func on_start_pressed():
+func _on_start_pressed():
 	get_tree().change_scene_to_file("res://maze.tscn")
 	
-func on_quit_pressed():
+func _on_quit_pressed():
 	get_tree().quit()
 
 func on_fade_finished():
